609e17cb3e8e4dc2a4dc5036cc6822e5b0f6dfa9
sayalipawade <sayalipawade05@gmail.com>
2020-04-12 21:48:35 +0530

    Added:Analyse the mood happy or sad

--- src/main/java/com/moodanalysis/MoodAnalyzer.java
+++ src/main/java/com/moodanalysis/MoodAnalyzer.java
@@ -1,8 +1,30 @@
 package com.moodanalysis;
 public class MoodAnalyzer
 {
+    private String message;
+
+    //main method
     public static void main(String[] args)
     {
         System.out.println("Welcome to mood analyzer");
+    }
+
+    // Method that return the mood
+    public String moodAnalyzer(String mood)
+    {
+        if (message.contains("sad"))
+        {
+            return ("sad");
+        } else if (message.contains("Any"))
+        {
+            return ("happy");
+        }
+        return (mood);
+    }
+
+    // Parameterized constructor
+    public MoodAnalyzer(String message)
+    {
+        this.message=message;
     }
 }
--- src/test/java/com/moodanalysis/MoodAnalyzerTestClass.java
+++ src/test/java/com/moodanalysis/MoodAnalyzerTestClass.java
@@ -1,6 +1,24 @@
 package com.moodanalysis;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MoodAnalyzerTestClass
 {
-
+    /* TC1.1:Given I am in sad mood message should return sad */
+    @Test
+    public void givenMeassage_WhenValid_ThenReturnMood()
+    {
+        MoodAnalyzer mood=new MoodAnalyzer("I am in sad mood");
+        String reaction=mood.moodAnalyzer("");
+        Assert.assertEquals("sad", reaction);
+    }
+    /* TC1.2:Given I am in Any mood message should return happy */
+    @Test
+    public void givenMeassage_WhenInvalid_ThenReturnMood()
+    {
+        MoodAnalyzer mood=new MoodAnalyzer("I am in Any mood");
+        String reaction=mood.moodAnalyzer("");
+        Assert.assertEquals("happy",reaction);
+    }
 }
+
